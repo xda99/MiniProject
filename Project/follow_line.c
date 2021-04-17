fac9db7ae58349574b6c4e7d028217b985f72908
David <dniederhauser@citycable.ch>
2021-04-18 00:08:59 +0200

    Commit files

---
+++ Project/follow_line.c
@@ -0,0 +1,47 @@
+#include "ch.h"
+#include "hal.h"
+#include <math.h>
+#include <usbcfg.h>
+#include <chprintf.h>
+
+
+#include <main.h>
+#include <motors.h>
+#include <pi_regulator.h>
+#include <process_image.h>
+
+
+static THD_WORKING_AREA(waLineFollow, 256);
+static THD_FUNCTION(LineFollow, arg) {
+
+    chRegSetThreadName(__FUNCTION__);
+    (void)arg;
+
+    systime_t time;
+    //computes the speed to give to the motors
+    int16_t speed = SPEED_EPUCK;
+    int16_t speed_correction = 0;
+
+    while(1){
+        time = chVTGetSystemTime();
+        
+        //computes a correction factor to let the robot rotate to be in front of the line
+        speed_correction = (get_line_position() - (IMAGE_BUFFER_SIZE/2));
+
+        //if the line is nearly in front of the camera, don't rotate
+        if(abs(speed_correction) < ROTATION_THRESHOLD){
+        	speed_correction = 0;
+        }
+
+        //applies the speed from the PI regulator and the correction for the rotation
+		right_motor_set_speed(speed - ROTATION_COEFF * speed_correction);
+		left_motor_set_speed(speed + ROTATION_COEFF * speed_correction);
+
+        //100Hz
+        chThdSleepUntilWindowed(time, time + MS2ST(10));
+    }
+}
+
+void line_follow_start(void){
+	chThdCreateStatic(waLineFollow, sizeof(waLineFollow), NORMALPRIO, LineFollow, NULL);
+}
---
+++ Project/follow_line.h
@@ -0,0 +1,7 @@
+#ifndef FOLLOW_LINE_H
+#define FOLLOW_LINE_H
+
+//start the thread to follow a line
+void line_follow_start(void);
+
+#endif /* FOLLOW_LINE_H */
