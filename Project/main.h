952002319d44d466b33d3ff2c8f9634441e11e68
Skitin <valentin.roch@epfl.ch>
2021-05-04 09:49:59 +0200

    Obstacle avoidance update

--- Project/main.h
+++ Project/main.h
@@ -17,9 +17,10 @@ extern "C" {
 #define ROTATION_THRESHOLD		10
 #define ROTATION_COEFF			2 
 #define PXTOCM					1570.0f //experimental value
-#define GOAL_DISTANCE 			10.0f
+#define GOAL_DISTANCE			10.0f
+#define IR_VALUE	 			60
 #define MAX_DISTANCE 			25.0f
-#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
+#define ERROR_THRESHOLD			5
 #define KP						800.0f
 #define KI 						3.5f	//must not be zero
 #define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
