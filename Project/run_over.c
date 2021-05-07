23de5920bf1f54714a13a2520a3f093198a12aea
David <dniederhauser@citycable.ch>
2021-05-07 16:39:42 +0200

    Run over au propre

--- Project/run_over.c
+++ Project/run_over.c
@@ -23,34 +23,6 @@ static bool obstacle=false;
 static bool obstacle_on_side=false;
 static bool left=false;
 
-/*
-void go_along(void)
-{
-	uint16_t prox_left=0;
-	uint16_t prox_right=0;
-	float ds=0.0f;
-	prox_left = (3*get_calibrated_prox(7) + 5*get_calibrated_prox(6) + get_calibrated_prox(5))/9;
-	prox_right = (3*get_calibrated_prox(0) + 5*get_calibrated_prox(1) + get_calibrated_prox(2))/9;
-
-	//obstacle on the right side
-	if(prox_right>prox_left)
-	{
-		ds = EPUCK_SPEED * (prox_right / IR_VALUE);
-		left_motor_set_speed(2*EPUCK_SPEED-ds);
-		right_motor_set_speed(ds);
-	}
-	else
-	{
-		ds = EPUCK_SPEED * (prox_left / IR_VALUE);
-		right_motor_set_speed(2*EPUCK_SPEED-ds);
-		left_motor_set_speed(ds);
-	}
-}
-*/
-
-
-
-
 void go_along(void)
 {
 	//playNote(440, 1000);
@@ -69,15 +41,8 @@ void go_along(void)
 	if((get_calibrated_prox(2)>IR_VALUE || get_calibrated_prox(5)>IR_VALUE) && !obstacle_on_side)
 	{
 		obstacle_on_side=true;
-		chprintf((BaseSequentialStream *)&SD3,"Obstacle\n");
+		//chprintf((BaseSequentialStream *)&SD3,"Obstacle\n");
 	}
-		//REGULATEUR
-		//while(get_colors()!=BLACK)
-		//{
-			//right_motor_set_speed(pi_regulator());
-			//left_motor_set_speed(pi_regulator());
-		//}
-
 	 obstacle=false;
 }
 
@@ -89,7 +54,7 @@ int16_t pi_regulator(void)
 	int16_t speed = 0;
 	static int16_t errord=0;
 	static int16_t sum_error = 0;
-	static systime_t time=0;
+//	static systime_t time=0;
 
 
 	if(left)
@@ -107,9 +72,7 @@ int16_t pi_regulator(void)
 		return 0;
 	}
 
-
 	sum_error += error;
-
 	//we set a maximum and a minimum for the sum to avoid an uncontrolled growth
 	if(sum_error > MAX_SUM_ERROR)
 	{
@@ -120,20 +83,24 @@ int16_t pi_regulator(void)
 		sum_error = -MAX_SUM_ERROR;
 	}
 
-	speed = KP*error+KD*(error-errord)/time;//+  sum_error;//KP * error;/* + KI * sum_error*/
+	speed = KP*error;	//+KD*(error-errord);	//+  sum_error;//KP * error;/* + KI * sum_error*/
 
-	chprintf((BaseSequentialStream *)&SD3,"Speed=%d\n", speed);
 	errord=error;
-	time = chVTGetSystemTime();
-
-	if(get_calibrated_prox(1)>IR_VALUE || get_calibrated_prox(6)>IR_VALUE)
+//	time = chVTGetSystemTime();
+/*
+	if(left && (get_calibrated_prox(1)>IR_VALUE && get_calibrated_prox(2)<10 && obstacle_on_side))
 	{
+		chprintf((BaseSequentialStream *)&SD3,"1\n");
 		speed=0;
 	}
-
+	else if(!left && (get_calibrated_prox(6)>IR_VALUE && get_calibrated_prox(5)<10 && obstacle_on_side))
+	{
+		chprintf((BaseSequentialStream *)&SD3,"2\n");
+		speed=0;
+	}
+*/
     return (int16_t)speed;
 }
-
 
 void rotation(int16_t direction)
 {
@@ -194,47 +161,24 @@ static THD_FUNCTION(Skirt, arg) {
 		if((get_calibrated_prox(2)>IR_VALUE|| get_calibrated_prox(5)>IR_VALUE) && !obstacle_on_side)
 		{
 			obstacle_on_side=true;
-			chprintf((BaseSequentialStream *)&SD3,"Obstacle\n");
 		}
 
-    	 if(obstacle_on_side)
-    	 {
-    		 speed_correction=pi_regulator();
-
+	    if(obstacle_on_side)
+	    {
+			 speed_correction=pi_regulator();
 			 //computes a correction factor to let the robot rotate to be in front of the line
 			 if(left)
 			 {
-//				 speed_correction = abs(IR_VALUE-get_calibrated_prox(2));
-//				 if(get_calibrated_prox(0)>get_calibrated_prox(3))
-//				 {
-//					 	chprintf((BaseSequentialStream *)&SD3,"1\n");
-						right_motor_set_speed(SPEED_EPUCK+speed_correction);
-						left_motor_set_speed(SPEED_EPUCK-speed_correction);
-/*				 }
-				 else
-				 {
-					 	chprintf((BaseSequentialStream *)&SD3,"2\n");
-						right_motor_set_speed(speed - ROTATION_COEFF * speed_correction);
-						left_motor_set_speed(speed + ROTATION_COEFF * speed_correction);
-				 }*/
+			  //chprintf((BaseSequentialStream *)&SD3,"1\n");
+				right_motor_set_speed(SPEED_EPUCK+speed_correction);
+				left_motor_set_speed(SPEED_EPUCK-speed_correction);
 			 }
 			 else
 			 {
-				// speed_correction = abs(IR_VALUE-get_calibrated_prox(5));
-				// if(get_calibrated_prox(7)>get_calibrated_prox(4))
-				// {
-					 //	chprintf((BaseSequentialStream *)&SD3,"speed=%d\n",speed_correction);
-						right_motor_set_speed(SPEED_EPUCK-speed_correction);
-						left_motor_set_speed(SPEED_EPUCK+speed_correction);
-				// }
-				 /*else
-				 {
-					 	chprintf((BaseSequentialStream *)&SD3,"4\n");
-						right_motor_set_speed(speed - ROTATION_COEFF * speed_correction);
-						left_motor_set_speed(speed + ROTATION_COEFF * speed_correction);
-				 }*/
+				right_motor_set_speed(SPEED_EPUCK-speed_correction);
+				left_motor_set_speed(SPEED_EPUCK+speed_correction);
 			 }
-    	 }
+	    }
     }
 }
 
